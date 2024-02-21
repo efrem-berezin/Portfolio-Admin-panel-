05846cf7c9beade3961ecc0ab82d947fdce1addd
Efrem Berezin <efrem430@gmail.com>
2024-02-21 16:59:50 -0600

    sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,18 @@
+import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
+import Widget from "../../components/widget/Widget"
 import "./home.scss"
  
 const Home = () => {
     return (
         <div className="home">
             <Sidebar/> 
-            <div className="homeContainer">container</div>       
+            <div className="homeContainer">
+                <Navbar/>
+                <div className="widgets">
+                    <Widget/>
+                </div>
+                 </div>       
         </div>
         
     )
